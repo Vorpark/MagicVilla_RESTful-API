f8677287257b74b8c01cf52c880a6c355f1abf0b
Vorpark <nik.vorpark@mail.ru>
2024-01-25 11:58:18 +0300

    Новая модель Villa и запрос GetVillas

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    [Route("api/VillaAPI")]
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa{Id = 1, Name = "Pool VIew"},
+                new Villa{Id = 2, Name = "Beach View"}
+            };
+        }
     }
 }
