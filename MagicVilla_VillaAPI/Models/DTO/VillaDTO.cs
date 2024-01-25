e42b994575a94ebbfa1040ca1d8648dd587b9f76
Vorpark <nik.vorpark@mail.ru>
2024-01-25 14:39:24 +0300

    Валидация с помощью ApiController

--- MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace MagicVilla_VillaAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_VillaAPI.Models.DTO
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
