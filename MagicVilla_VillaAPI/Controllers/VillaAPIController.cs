606a5251e3bec9167dc62d470ffcdda6199d84fe
Vorpark <nik.vorpark@mail.ru>
2024-01-25 12:32:04 +0300

    Новый запрос GetVilla

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -13,5 +13,11 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+        }
     }
 }
