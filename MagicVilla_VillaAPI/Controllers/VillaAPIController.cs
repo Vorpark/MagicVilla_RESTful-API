2e61338191319b52ddf7eb84305c3380235f6f20
Vorpark <nik.vorpark@mail.ru>
2024-01-25 12:11:21 +0300

    Реализация DTO

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,4 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
@@ -8,12 +8,12 @@ namespace MagicVilla_VillaAPI.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        public IEnumerable<VillaDTO> GetVillas()
         {
-            return new List<Villa>()
+            return new List<VillaDTO>()
             {
-                new Villa{Id = 1, Name = "Pool VIew"},
-                new Villa{Id = 2, Name = "Beach View"}
+                new VillaDTO{Id = 1, Name = "Pool VIew"},
+                new VillaDTO{Id = 2, Name = "Beach View"}
             };
         }
     }
---
+++ MagicVilla_VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_VillaAPI.Models.DTO
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
