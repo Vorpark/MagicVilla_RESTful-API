500bb1be05b13ffce11dee421d3f750e8a133685
Vorpark <nik.vorpark@mail.ru>
2024-01-26 11:14:54 +0300

    Подключение и настройка EF Core

---
+++ MagicVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        { 
+
+        }
+    }
+}
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,6 +1,12 @@
+using MagicVilla_VillaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
